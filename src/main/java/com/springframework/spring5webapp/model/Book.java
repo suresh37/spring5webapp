4c5b8fc36a3e7b914972e4860b7c9f8469f39da6
suresh37 <sureshkarthick37@gmail.com>
2018-07-23 22:19:19 +0530

    Publisher class added - DevBootstrap class updated

--- src/main/java/com/springframework/spring5webapp/model/Book.java
+++ src/main/java/com/springframework/spring5webapp/model/Book.java
@@ -22,10 +22,18 @@ public class Book {
     {
 
     }
+
     public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
         this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
     }
 
     public Long getId() {
